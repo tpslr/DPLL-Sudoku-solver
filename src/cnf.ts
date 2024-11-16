45d8033d96135fb29b698e45f43c6a063a993548
tpslr <topias.lyyra@gmail.com>
2024-11-16 20:51:46 +0000

    Moved cnf file parsing to it's own file

---
+++ src/cnf.ts
@@ -0,0 +1,83 @@
+
+
+function parseCNF(cnf: string) {
+
+    // extract metadata from the "p cnf <variables> <clauses>" line
+    const metadata = cnf.matchAll(/^p cnf (\d+) (\d+)/mg).next().value;
+
+    if (!metadata) throw new Error("Invalid input!, missing 'p cnf <variables> <clauses>' line.");
+
+    const variableCount = parseInt(metadata[1]);
+    const clauseCount = parseInt(metadata[2]);
+
+    const clauseLen = ((((variableCount >> 6) + 1) << 6)) * 2;
+
+    const lines = cnf.split(/\r\n|\r|\n/g);
+
+    const clausesArray: Buffer[] = [];
+
+
+    for (const line of lines) {
+        // line doesn't begin with number or dash, skip
+        if (line.match(/^[^-\d]/)) continue;
+
+        const buffer = Buffer.alloc(clauseLen / 8);
+
+        for (const symbol of line.split(" ")) {
+            const negate = symbol.startsWith("-");
+            if (symbol === "0") continue;
+            const symbolNumber = Math.abs(parseInt(symbol)) - 1;
+
+            const index64 = symbolNumber >> 6 << 6;
+            const start = index64 * 2;
+            const offset = symbolNumber & 63;
+
+            setBufferSymbol(buffer, start + offset, true);
+            setBufferSymbol(buffer, start + offset + 64, negate);
+        }
+
+        clausesArray.push(buffer);
+    }
+
+    return {
+        clauses: clausesArray,
+        variableCount,
+        clauseCount,
+    };
+}
+
+
+function parseSolution(solution: Buffer, variables: number) {
+    let out = "";
+    for (let i = 0; i < variables; i++) {
+        const prefix = getBufferSymbol(solution, i) == 0
+            ? "-"
+            : "";
+
+        out += `${prefix}${i + 1} `;
+    }
+    return out + "0";
+}
+
+
+function setBufferSymbol(buffer: Buffer, bit: number, value: boolean) {
+    const index = bit >> 3;
+    bit = bit & 7;
+    if (value) {
+        buffer[index] |= 1 << bit;
+    } else {
+        buffer[index] &= ~(1 << bit);
+    }
+}
+
+function getBufferSymbol(buffer: Buffer, bit: number) {
+    const index = bit >> 3;
+    bit = bit & 7;
+    return (buffer[index] & 1 << bit) >> bit;
+}
+
+
+export {
+    parseCNF,
+    parseSolution
+};
--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,26 @@
-import { readFile, writeFile } from "fs/promises"
-import DPLL from "DPLL"
+import { readFile } from "fs/promises";
+import DPLL from "DPLL";
+import { parseCNF, parseSolution } from "./cnf.js";
 
+// import "./sudoku.js";
 
-
-async function loadCNF(path: string) {
+/**
+ * Solves a conjuctive normal form SAT problem in a file
+ * @param path File path to CNF SAT problem
+ * @returns Solution to CNF SAT problem or "UNSATISFIABLE"
+ */
+async function solveCNF(path: string) {
     const data = await readFile(path, "utf-8");
 
-    // extract metadata from the "p cnf <variables> <clauses>" line
-    const metadata = data.matchAll(/^p cnf (\d+) (\d+)/mg).next().value;
-    if (!metadata) throw new Error("Invalid input!, missing 'p cnf <variables> <clauses>' line.");
+    const cnf = parseCNF(data);
 
-    const variables = parseInt(metadata[1]);
-    const clauses = parseInt(metadata[2]);
+    const solution = DPLL.solve(cnf.variableCount, cnf.clauses);
 
-    const clauseLen = ((((variables >> 6) + 1) << 6)) * 2;
-    console.log(`Variables: ${variables}`);
-    console.log(`Clauses: ${clauses}`);
-
-    const lines = data.split(/\r\n|\r|\n/g);
-
-
-    const clausesArray: Buffer[] = [];
-
-    
-    for (const line of lines) {
-        // line doesn't begin with number or dash, skip
-        if (line.match(/^[^-\d]/)) continue;
-
-        const buffer = Buffer.alloc(clauseLen / 8);
-
-        for (const symbol of line.split(" ")) {
-            const negate = symbol.startsWith("-");
-            if (symbol === "0") continue;
-            const symbolNumber = Math.abs(parseInt(symbol)) - 1;
-
-            const index64 = symbolNumber >> 6 << 6;
-            const start = index64 * 2;
-            const offset = symbolNumber & 63;
-
-            console.log(`${start + offset}, ${start + offset + 64}`);
-
-            setBufferSymbol(buffer, start + offset, true);
-            setBufferSymbol(buffer, start + offset + 64, negate);
-        }
-
-        clausesArray.push(buffer)
-
+    if (solution === "UNSATISFIABLE") {
+        return solution;
     }
-    const solution = DPLL.solve(variables, clausesArray);
-    if (typeof solution === "string") {
-        console.log(solution);
-        return;
-    }
-    let out = "";
-    let out2 = "";
-    for (let i = 0; i < variables; i++) {
-        console.log(`Var ${i + 1} = ${getBufferSymbol(solution, i)}`);
-        out += `Var ${i + 1} = ${getBufferSymbol(solution, i)}\n`;
-        out2 += `${getBufferSymbol(solution, i) == 0 ? "-" : ""}${i + 1} 0\n`;
-    }
-    writeFile("out1.txt", out);
-    writeFile("out2.txt", out2)
-    debugger;
+
+    return parseSolution(solution, cnf.variableCount);
 }
 
-function setBufferSymbol(buffer: Buffer, bit: number, value: boolean){
-    const index = bit >> 3;
-    bit = bit & 7;
-    if(value) {
-        buffer[index] |= (1 << bit);
-    }
-    else {
-        buffer[index] &= ~(1 << bit);
-    }
-}
-function getBufferSymbol(buffer: Buffer, bit: number){
-    const index = bit >> 3;
-    bit = bit & 7;
-    return (buffer[index] & (1 << bit)) >> bit;
-}
-
-
-loadCNF("./add4.cnf")
+export { solveCNF };
