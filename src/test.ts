ed4010c5615808815407abbdf721d80ad68467ff
tpslr <topias.lyyra@gmail.com>
2024-11-20 18:08:21 +0000

    Run all tests

--- src/test.ts
+++ src/test.ts
@@ -61,4 +61,21 @@ async function cnfTests(dir: string) {
     }
 }
 
-cnfTests("./tests/cnf");
+
+const tests = await readdir("./dist/tests");
+
+const only = process.argv[2];
+
+for (const test of tests) {
+    if (!test.endsWith(".js")) continue;
+
+    if (only && test.replace(".js", "") !== only) continue; 
+
+    console.info(`Running tests ${test}...`);
+    console.info(joinPath(import.meta.dirname, "tests", test));
+    import(joinPath(import.meta.dirname, "tests", test));
+}
+
+if (!only || only === "cnf") {
+    cnfTests("./tests/cnf");
+}
